837207269f37f5fc8fd9f7e5941e96603e05a105
Donato Rimenti <donatohan.rimenti@gmail.com>
2021-05-25 23:50:13 +0200

    First commit.

---
+++ app.js
@@ -0,0 +1,14 @@
+function sadMikso() {
+    new Audio("audio/sad_mikso.ogg").play();
+    $("#mikso").attr("src", "img/sad_patate.jpg");
+}
+
+function surprisedMikso() {
+    new Audio("audio/surprised_mikso.ogg").play();
+    $("#mikso").attr("src", "img/surprised_patate.jpg");
+}
+
+function happyMikso() {
+    new Audio("audio/happy_mikso.ogg").play();
+    $("#mikso").attr("src", "img/happy_carote.jpg");
+}
